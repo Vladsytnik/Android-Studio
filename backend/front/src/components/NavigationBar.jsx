cd634a6fff2d08aa30f441024e74788160cb7187
Vladsytnik <sytnik.vlad2014@yandex.ru>
2021-05-19 10:53:30 +0300

    ostalas eshe 1 laba

--- backend/front/src/components/NavigationBar.jsx
+++ backend/front/src/components/NavigationBar.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Navbar, Nav} from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faHome, faUser} from '@fortawesome/free-solid-svg-icons';
+import {faBars, faHome} from '@fortawesome/free-solid-svg-icons';
 import {Link, withRouter} from "react-router-dom";
+import {faUser} from "@fortawesome/free-solid-svg-icons/faUser";
 import Utils from "../utils/Utils";
 import BackendService from "../services/BackendService";
-
+import {connect} from "react-redux";
+import {userActions} from "../utils/Rdx";
 
 class NavigationBar extends React.Component {
 
@@ -31,25 +33,42 @@ class NavigationBar extends React.Component {
         let uname = Utils.getUserName();
         return (
             <Navbar bg="light" expand="lg">
-                <Navbar.Brand><FontAwesomeIcon icon={faHome}/>{' '}myRPO</Navbar.Brand>
+                <button type="button"
+                        className="btn btn-outline-secondary mr-2"
+                        onClick={this.props.toggleSideBar}>
+                    <FontAwesomeIcon icon={faBars}/>
+                </button>
+                <Navbar.Brand>myRPO</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
+                        {/*<Nav.Link href="/home">Home</Nav.Link>*/}
                         <Nav.Link as={Link} to="/home">Home</Nav.Link>
                         <Nav.Link onClick={this.goHome}>Another home</Nav.Link>
                         <Nav.Link onClick={()=>{this.props.history.push("/home")}}>Yet another home</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
-                <Navbar.Text>{uname}</Navbar.Text>
-                {uname &&
+                <Navbar.Text>{this.props.user && this.props.user.name}</Navbar.Text>
+                {this.props.user &&
                 <Nav.Link onClick={this.logout}><FontAwesomeIcon icon={faUser} fixedWidth/>{' '}Выход</Nav.Link>
                 }
-                {!uname &&
+                {!this.props.user &&
                 <Nav.Link as={Link} to="/login"><FontAwesomeIcon icon={faUser} fixedWidth/>{' '}Вход</Nav.Link>
                 }
             </Navbar>
         );
     }
+
+    logout() {
+        BackendService.logout().finally(() => {
+            this.props.dispatch(userActions.logout())
+            this.props.history.push('/login')
+        })
+    }
 }
 
-export default withRouter (NavigationBar);
+function mapStateToProps(state){
+    const {user} = state.authentication;
+    return {user};
+}
+export default connect(mapStateToProps)(withRouter(NavigationBar));
